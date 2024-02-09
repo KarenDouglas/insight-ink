9f549ba1bf7dee3531d46519e8687a9ce584d7f8
Karen Douglas <kdindependence@gmail.com>
2024-02-09 14:35:50 -0600

    created basic form for new entry wit click listener

--- public/js/entries.js
+++ public/js/entries.js
@@ -6,3 +6,27 @@
 // render past entries
 // dummy data
 
+const $newEntryTitleInput = document.querySelector('#newEntryTitle')
+const $newEntryTextArea = document.querySelector('#newEntry')
+
+const $newHabitCheckboxes = document.querySelectorAll('input[name="habitToBeTracked"]:checked')
+const $newEntrySubmitButton = document.querySelector('#newEntrySubmitButton')
+
+const $newMoodSelect = document.querySelector('#moodSelect')
+
+
+const submitNewEntry = (e) => {
+    e.preventDefault()
+const newSubmittedEntry = {
+    title: $newEntryTitleInput.value,
+    description:$newEntryTextArea.value,
+    mood:$newMoodSelect.value
+}
+
+    
+console.log(newSubmittedEntry)
+} 
+
+$newEntrySubmitButton.addEventListener('click', submitNewEntry)
+
+
