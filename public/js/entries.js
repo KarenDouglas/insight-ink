a82cd302e33cc0e2543daa32f00c80836269f2a2
Karen Douglas <kdindependence@gmail.com>
2024-02-09 11:15:19 -0600

    file for new entry form set up

---
+++ public/js/entries.js
@@ -0,0 +1,8 @@
+// a list of past entries
+// newest entry prominent
+// click listener for each past entry item
+//click lister for each new entry
+// new entry button
+// render past entries
+// dummy data
+
