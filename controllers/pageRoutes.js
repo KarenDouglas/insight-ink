9f658b8b76e2075432f2042d129f8f3f44381243
AnnaC0rmier <anna.cormier.96@gmail.com>
2024-02-09 14:41:55 -0600

    added entry view

--- controllers/pageRoutes.js
+++ controllers/pageRoutes.js
@@ -2,7 +2,28 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async(req, res)=>{
-    res.render('layouts/main')
+    const data = {
+        pageTitle: 'Dash',
+        buttons: [
+            {text: 'NEW ENTRY', url: '/new-entry' },
+            {text: 'HABITS', url: '/habits'},
+        ],
+    }
+    res.render('layouts/main', data)
 })
+
+router.get('/new-entry', async (req, res) => {
+    const data = {
+        pageTitle: 'Entry',
+        buttons: [
+            { text: 'ENTRIES', url: '/' },
+            { text: 'HABITS', url: '/habits' },
+        ],
+    };
+
+    res.render('entry', data);
+});
+
+
 
 module.exports = router;
